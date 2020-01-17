fb4bedf5ec3f078b4fe0586d19e93d62da18b4a7
Éderson Fernandes <edersonluis.rf@gmail.com>
2020-01-17 16:34:51 -0300

    Aula 5

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const http = require('http');
 
 const routes = require('./routes');
 
+const { setupWebsocket } = require('./websocket');
+
 const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-76qu3.mongodb.net/week10?retryWrites=true&w=majority', {
     useNewUrlParser: true,
@@ -19,4 +25,4 @@ app.use(express.json());
 
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -5,6 +5,7 @@ import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
 import { MaterialIcons } from '@expo/vector-icons';
 
 import api from '../services/api';
+import { connect, disconnect, subscribeToNewDevs } from '../services/socket';
 
 function Main({ navigation }) {
     const [devs, setDevs] = useState([]);
@@ -34,12 +35,24 @@ function Main({ navigation }) {
         loadInitialPosition();
     }, []);
 
+    useEffect(() => {
+        subscribeToNewDevs(dev => setDevs([...devs, dev]));
+    }, [devs]);
+
     if (!currentRegion) {
         return null;
     }
 
     function handleRegionChanged(region) {
         setCurrentRegion(region);
+    }
+
+    function setupWebsocket() {
+        disconnect();
+
+        const { latitude, longitude } = currentRegion;
+
+        connect(latitude, longitude, techs);
     }
 
     async function loadDevs() {
@@ -54,6 +67,7 @@ function Main({ navigation }) {
         });
 
         setDevs(response.data.devs);
+        setupWebsocket();
     }
 
     return (
